f3d18177f34fa41304f63c157618b6d5fb3d5d76
BartG1014 <idxrpp@gmail.com>
2026-05-27 21:51:19 +0300

    Add Arabic localization support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,7 @@ export default defineNuxtConfig({
 		locales: [
 			{ code: 'en', language: 'en-US', file: 'en.json' },
 			{ code: 'pt-br', language: 'pt-BR', file: 'pt-br.json' },
+			{ code: 'ar', language: 'ar-MA', file: 'ar.json' },
 			{ code: 'sp', language: 'es-ES', file: 'sp.json' },
 		],
 		defaultLocale: 'en',
